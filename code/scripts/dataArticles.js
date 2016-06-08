22e3e5963eafbed4843213c0ddb5b988a3a3ec13
Jennifer Liu <jenniferliu716@gmail.com>
2016-06-08 00:46:27 -0700

    input ourLocalData @dataArticles.js and add structure @index.html

---
+++ code/scripts/dataArticles.js
@@ -0,0 +1,18 @@
+var ourLocalData = [
+  {
+    place:       'London',
+    guideUrl:    'http://travel.usnews.com/London_England/',
+    body:        '<p>The English writer Samuel Johnson famously said, "You find no man, at all intellectual, who is willing to leave London. No, Sir, when a man is tired of London, he is tired of life; for there is in London all that life can afford." More than two centuries have passed since Johnson\'s era, but his words still ring true. Life in London is nothing short of invigorating, and travelers find that one visit isn\'t enough to experience everything this two-millennia-old city has to offer.</p>'
+  },
+  {
+    place:       'Paris',
+    guideUrl:    'http://travel.usnews.com/Paris_France/',
+    body:        '<p>The City of Light draws millions of visitors every year with its unforgettable ambiance. Of course, the divine cuisine and vast art collections deserve some of the credit as well. The gentle River Seine rambles through the city, flanked by stately museums, centuries-old churches, and blocks of Rococo- and Neoclassic-design architecture, further enhanced by charming trees and glowing streetlamps. Peppering the Seine\'s cobbled walks and graceful bridges are impossibly chic Parisians, probably on their way to the market, cafe or cinema.</p>'
+  },
+  {
+    place:       'Sydney',
+    guideUrl:    'http://travel.usnews.com/Sydney_Australia/',
+    body:        '<p>Sydney is both a laid-back beachside town and a thriving metropolis that boasts some of the Southern Hemisphere\'s best surf, landmarks and activities. Whether you\'re looking to watch a show at the iconic Opera House, take to the waves at Bondi Beach or explore trendy areas like The Rocks and Darling Harbour, Sydney features something for everyone. Even Sydneysiders have an ideal mix of both worlds: Fashion-forward attire and British-style sarcasm combine with a "no worries" attitude and relaxed coastal vibe. It\'s no wonder this vibrant city down under is a natural choice for first-time Aussie visitors.</p>'
+  },
+
+];
